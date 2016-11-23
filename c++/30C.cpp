fdf4f79103b98d7b928c108059d54457ce5b9daf
projectormato <k.keita811.anuy@gmail.com>
2016-11-23 16:09:56 +0900

    change host

---
+++ c++/30C.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a_b, b_a, atime, btime;
+	cin >> a_b >> b_a >> atime >> btime;
+	int ago[a_b];
+	int bgo[b_a];
+	for (int i = 0; i < a_b; i++)
+	{
+		cin >> ago[i];
+	}
+}
