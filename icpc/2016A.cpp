5b132c4fc6aff4a35024a01fe96d293cbbcc3bb0
projectormato <k.keita811.anuy@gmail.com>
2017-04-28 17:40:14 +0900

    add icpc code

---
+++ icpc/2016A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+	int n;
+	vector<int> a;
+	while(true){
+		cin >> n;
+		if (n == 0){
+			break;
+		}
+		cout << n << endl;
+	}
+
+}
