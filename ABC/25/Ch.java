30696efeb52f7a414802ffb872023fa5a500957c
projectormato <k.keita811.anuy@gmail.com>
2017-09-20 21:23:24 +0900

    add java codes

---
+++ ABC/25/Ch.java
@@ -0,0 +1,11 @@
+class Ch{
+	public static void main(String[] args) {
+		// String n = "abcde";
+		// System.out.println( n.charAt(1) + n.charAt(2) );
+		char a = 'a';
+		char b = 'b';
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(a + b);
+	}
+}
---
+++ Ruby/getstest.rb
@@ -0,0 +1,3 @@
+n = gets.to_i
+input = (1..n).map { gets.chomp.split(' ') }
+p input
