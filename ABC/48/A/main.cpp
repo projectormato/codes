bf2dec45186077ca45d13760f6b14458dcb06205
projectormato <k.keita811.anuy@gmail.com>
2019-05-06 11:34:49 +0900

    add ABC048A.

---
+++ ABC/48/A/main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define step(i, s, n, d) for(int i=s; i<n; i+=d)
+#define FOR(i,s,n) step(i,s,n,1)
+#define rep(i,n) FOR(i,0,n)
+#define ll long long
+typedef pair<int, int> P;
+
+int main(){
+  string s1, s2, s3;
+  cin >> s1 >> s2 >> s3;
+  cout << s1[0] << s2[0] << s3[0] << endl;
+}
