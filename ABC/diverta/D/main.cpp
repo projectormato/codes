3291e7cd38a648eaa8e012af42f88c3a844a8f36
projectormato <k.keita811.anuy@gmail.com>
2019-05-12 01:01:38 +0900

    fix diverta D.

--- ABC/diverta/D/main.cpp
+++ ABC/diverta/D/main.cpp
@@ -9,10 +9,19 @@ typedef pair<int, int> P;
 int main(){
   ll n, ans = 0;
   cin >> n;
-  for(ll i = 1;  i < sqrt(n); i++) {
+  for(ll i = 1;  i*i <= n; i++) {
     // cout << n/m << " " << n%m << endl;
     if (n%i == 0) {
-      ans += (n/i)-1;
+      if (i > 1) {
+        if (n%(i-1) == n/(i-1)) {
+          ans += i-1;
+        }
+      }
+      if (n/i-1 > 1) {
+        if (n%(n/i-1) == n/(n/i-1)) {
+          ans += (n/i-1);
+        }
+      }
       // cout << (n/i)-1 << endl;
     }
   }
