a894d5bc714a375fb4247086062b04eb02b8dea3
projectormato <k.keita811.anuy@gmail.com>
2021-01-23 23:23:40 +0900

    add dijkstra

--- algorithm/dijkstra/main.cpp
+++ algorithm/dijkstra/main.cpp
@@ -10,6 +10,7 @@ typedef map<int,int> M;
 
 // ref: https://cpprefjp.github.io/
 // http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_A&lang=ja
+
 int main(){
     ll INF = 100000001;
     // V: 頂点の数 E: 辺の数 r: 始点
@@ -22,7 +23,7 @@ int main(){
         cin >> d[i][0] >> d[i][1] >> d[i][2];
     }
 
-    rep(i, E) {
+    rep(i, V) {
         ans[i] = INF;
     }
     rep(i, V) {
@@ -30,29 +31,49 @@ int main(){
     }
     ans[r] = 0;
 
-    // rep(i ,E) {
+    rep(i ,V) {
         // checkedに含まれておらず、ansが最も小さい頂点を探す
         ll target = INF;
-        rep(i, V-1) {
-            if(ans[i] <= ans[i+1]) {
-                target = i;
+        rep(j, V) {
+            if (checked[j]) {
+                continue;
+            }
+            if (target == INF) {
+                target = j;
+            }
+            if(ans[j] < ans[target]) {
+                target = j;
             }
         }
+        cout << "今回のターゲット: " << target << endl;
         checked[target] = true;
 
-        rep(i, V) {
-            if(checked[i]) {
-
+        rep(j, E) {
+            // targetと隣接していて、まだcheckされていない辺のコストを更新する
+            if(!checked[d[j][1]] && d[j][0] == target) {
+                ans[d[j][1]] = min(ans[d[j][1]], ans[target] + d[j][2]);
             }
+
+            // 辺が無向ならこのif文も必要(双方向で隣接しているかチェックする)
+            // if(!checked[d[j][0]] && d[j][1] == target) {
+            //     ans[d[j][0]] = min(ans[d[j][0]], ans[target] + d[j][2]);
+            // }
         }
+        cout << i <<  "回目終わり時点のans: ";
+        rep(j, V) {
+            cout << ans[j] << " ";
+        }
+        cout << endl;
 
-    // }
-
-
-    rep(i, E) {
-        cout << d[i][0] << " " << d[i][1] << " " <<  d[i][2] << endl;
     }
-
+    
+    // rep(i, V) {
+    //     if(ans[i] == INF) {
+    //         cout << "INF" << endl;   
+    //     } else {
+    //     cout << ans[i] << endl;
+    //     }
+    // }
 }
 
 
